ff37c92fe1736a7f4a762fd5bb77bf4be7dba5d0
Paul Durbaba <paul.durbaba@btinternet.com>
2014-05-26 10:16:05 +0100

    Added Vault stuff

--- RewardsTime/src/me/ithinkrok/rewardstime/RewardsTime.java
+++ RewardsTime/src/me/ithinkrok/rewardstime/RewardsTime.java
@@ -1,11 +1,36 @@
 package me.ithinkrok.rewardstime;
 
+import net.milkbowl.vault.economy.Economy;
+
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class RewardsTime extends JavaPlugin {
+	
+	public static boolean log = true;
 
+	public static Economy economy = null;
+	
+	
 	@Override
 	public void onEnable() {
-		
+		setupEconomy();
+		if(log) getLogger().info("Loaded plugin RewardsTime version: " + getDescription().getVersion());
 	}
+	
+	@Override
+	public void onDisable() {
+		economy = null;
+		if(log) getLogger().info("Unloaded RewardsTime");
+	}
+	
+	private boolean setupEconomy()
+    {
+        RegisteredServiceProvider<Economy> economyProvider = getServer().getServicesManager().getRegistration(net.milkbowl.vault.economy.Economy.class);
+        if (economyProvider != null) {
+            economy = economyProvider.getProvider();
+        }
+
+        return (economy != null);
+    }
 }
