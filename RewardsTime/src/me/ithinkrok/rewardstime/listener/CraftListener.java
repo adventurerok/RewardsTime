47f75a186e86f4bf5564a04570108b69d149fdcf
Paul Durbaba <paul.durbaba@btinternet.com>
2014-05-28 10:58:45 +0100

    Fixed hotbar button furnace expoit

--- RewardsTime/src/me/ithinkrok/rewardstime/listener/CraftListener.java
+++ RewardsTime/src/me/ithinkrok/rewardstime/listener/CraftListener.java
@@ -25,42 +25,6 @@ public class CraftListener implements Listener {
 	public void onCraftItem(CraftItemEvent event){
 		if(event.isCancelled()) return;
 		if(!plugin.craftRewards) return;
-		if(!(event.getWhoClicked() instanceof Player)) return;
-		String item = event.getCurrentItem().getType().toString().toLowerCase();
-		Player player = (Player) event.getWhoClicked();
-		if(!plugin.enabledGameModes.get(player.getGameMode())) return;
-		double amount = plugin.config.getDouble("craft." + item + ".money", 0);
-		String withMeta = "craft." + item + "/" + event.getCurrentItem().getDurability() +".money";
-		if(plugin.config.contains(withMeta)){
-			amount = plugin.config.getDouble(withMeta);
-		}
-		
-		//event.getCurrentItem().getAmount() is broken as it returns number of items shown in results slot
-		amount *= event.getCurrentItem().getAmount();
-		
-		event.getCurrentItem();
-		
-		String dropsStr = plugin.config.getString("craft." + item + ".items");
-			Collection<ItemStack> result = plugin.computeDrops(dropsStr, event.getCurrentItem().getAmount());
-			plugin.givePlayerItems(player, result.toArray(new ItemStack[result.size()]));
-		
-		player.giveExp(event.getCurrentItem().getAmount() * plugin.getConfig().getInt("craft." + item + ".exp", 0));
-		
-		if(amount == 0) return;
-		if(amount > 0){
-			plugin.playerReward(player, amount, 0, 0);
-		} else if(amount < 0){
-			plugin.playerReward(player, 0, 0, -amount);
-		}
-		
-		
-		
-	}
-	
-	@EventHandler(priority = EventPriority.MONITOR)
-	public void onInventoryClick(InventoryClickEvent event){
-		if(!plugin.smeltRewards) return;
-		if(event.getInventory().getType() != InventoryType.FURNACE) return;
 		if(event.getWhoClicked() == null || !(event.getWhoClicked() instanceof Player)) return;
 		if(event.getSlotType() != SlotType.RESULT) return;
 		final Player player = (Player) event.getWhoClicked();
@@ -87,16 +51,18 @@ public class CraftListener implements Listener {
 		if(itemAmount <= 0) return;
 		
 		String item = event.getCurrentItem().getType().toString().toLowerCase();
-		double amount = plugin.config.getDouble("smelt." + item + ".money", 0);
-		String withMeta = "smelt." + item + "/" + event.getCurrentItem().getDurability() +".money";
+		String base = "craft." + item;
+		double amount = plugin.config.getDouble(base + ".money", 0);
+		String withMeta = base + "/" + event.getCurrentItem().getDurability();
 		if(plugin.config.contains(withMeta)){
-			amount = plugin.config.getDouble(withMeta);
+			amount = plugin.config.getDouble(withMeta + ".money");
+			base = withMeta;
 		}
 		amount *= itemAmount;
 		
 		final int itemAmountFinal = itemAmount;
 		
-		final String dropsStr = plugin.config.getString("smelt." + item + ".items");
+		final String dropsStr = plugin.config.getString(base + ".items");
 		Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
 			
 			@Override
@@ -107,7 +73,63 @@ public class CraftListener implements Listener {
 		});
 		
 		
-		player.giveExp(itemAmount * plugin.getConfig().getInt("smelt." + item + ".exp", 0));
+		player.giveExp(itemAmount * plugin.getConfig().getInt(base + ".exp", 0));
+		
+		if(amount == 0) return;
+		if(amount > 0){
+			plugin.playerReward(player, amount, 0, 0);
+		} else if(amount < 0){
+			plugin.playerReward(player, 0, 0, -amount);
+		}
+		
+	}
+	
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void onInventoryClick(InventoryClickEvent event){
+		if(event.isCancelled()) return;
+		if(!plugin.smeltRewards) return;
+		if(event.getWhoClicked() == null || !(event.getWhoClicked() instanceof Player)) return;
+		if(event.getSlotType() != SlotType.RESULT) return;
+		final Player player = (Player) event.getWhoClicked();
+		if(!plugin.enabledGameModes.get(player.getGameMode())) return;
+		
+		int playerAmount = player.getItemOnCursor().getAmount();
+		int slotAmount = event.getCurrentItem().getAmount();
+		int itemAmount = 0;
+		if(slotAmount == 0) return;
+		if(event.isShiftClick() || event.getHotbarButton() != -1){
+			itemAmount = plugin.getFittingAmount(event.getCurrentItem(), player.getInventory());
+		} else if(player.getItemOnCursor().getType() == Material.AIR){
+			itemAmount = slotAmount;
+		} else if(player.getItemOnCursor().isSimilar(event.getCurrentItem())){
+			itemAmount = Math.min(slotAmount, event.getCurrentItem().getMaxStackSize() - playerAmount);
+		}
+		if(itemAmount <= 0) return;
+		
+		String item = event.getCurrentItem().getType().toString().toLowerCase();
+		String base = "smelt." + item;
+		double amount = plugin.config.getDouble(base + ".money", 0);
+		String withMeta = base + "/" + event.getCurrentItem().getDurability();
+		if(plugin.config.contains(withMeta)){
+			amount = plugin.config.getDouble(withMeta + ".money");
+			base = withMeta;
+		}
+		amount *= itemAmount;
+		
+		final int itemAmountFinal = itemAmount;
+		
+		final String dropsStr = plugin.config.getString(base + ".items");
+		Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
+			
+			@Override
+			public void run() {
+				Collection<ItemStack> result = plugin.computeDrops(dropsStr, itemAmountFinal);
+				plugin.givePlayerItems(player, result.toArray(new ItemStack[result.size()]));
+			}
+		});
+		
+		
+		player.giveExp(itemAmount * plugin.getConfig().getInt(base + ".exp", 0));
 		
 		if(amount == 0) return;
 		if(amount > 0){
